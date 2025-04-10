64b8fcf769ab2e20f3a03ed0852fac53379b7f32
leandrocorso <bolognacorsoleandro@gmail.com>
2025-04-10 15:38:29 -0300

    third commit

--- venv/app.py
+++ venv/app.py
@@ -5,33 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return """
-  <body style="background-color: #87CEEB; margin: 0; padding: 0; font-family: Arial, sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh;">
-        <div style="text-align: center; color: white;">
-            <h1>HomePage</h1>
-            <p>Bem-vindo ao meu portfólio!</p>
-            <a href="/contatos">
-                <button style="padding: 10px 20px; font-size: 16px; cursor: pointer; background-color: #4CAF50; color: white; border: none; border-radius: 5px;">
-                    Ir para Contatos
-                </button>
-            </a>
-        </div>
-    </body>
-    """
+    return render_template("index.html")
 
 @app.route("/contatos")
 def about():
-    return """
-    <body style="background-color: #f5f5f5; margin: 0; padding: 0; font-family: Arial, sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh;">
-        <div style="text-align: center; color: #333;">
-            <h1>Contatos</h1>
-            <p><strong>Número:</strong> (11) 94087-6963</p>
-            <p><strong>E-mail:</strong> bolognacorsoleandro@gmail.com</p>
-            <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/leandro-bologna-corso/" target="_blank" style="color: #0077b5;">https://www.linkedin.com/in/leandro-bolognacorso/</a></p>
-            <p><a href="/static/curriculo.pdf" target="_blank" style="color: #4CAF50; text-decoration: none; font-weight: bold;">Baixar Currículo (PDF)</a></p>
-        </div>
-    </body>
-    """
+    return render_template("contatos.html")
 
 
 if __name__ == "__main__":
